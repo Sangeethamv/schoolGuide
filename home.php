9e278c1a12f2e269e10cd83e2f868f669b04e361
sangeethamv <sangeethamv@gmail.com>
2015-09-14 10:37:55 +0530

    adding home.php

---
+++ home.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+<title>SchoolGuide</title>
+	<?php
+	$profpic = "Images/gurukul.png";
+	?>
+
+<style type="text/css">
+body {
+	background-image: url('<?php echo $profpic;?>');
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+	width: 100%;
+	height: 100%;
+}
+</style>
+</head>
+
+</html>
+
+
+
