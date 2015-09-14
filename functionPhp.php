1973d9500fcf59bc8d6bf62f59e7e253db236bf7
sangeethamv <sangeethamv@gmail.com>
2015-09-14 10:38:49 +0530

    adding functionPhp

---
+++ functionPhp.php
@@ -0,0 +1,21 @@
+<?php
+
+function sum($x, $y) {
+    $z = $x + $y;
+    return $z;
+}
+
+echo "5 + 10 = " . sum(5, 10) . "<br>";
+echo "7 + 13 = " . sum(7, 13) . "<br>";
+echo "2 + 4 = " . sum(2, 4). "<br>";
+
+
+function familyName($fname, $year) {
+	echo "$fname Refsnes. Born in $year <br>";
+}
+
+familyName("Hege", "1975");
+familyName("Stale", "1978");
+familyName("Kai Jim", "1983");
+
+?>
