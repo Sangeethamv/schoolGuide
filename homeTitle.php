ebacddda4eb7f096fd0fa1cdf1b300521584624d
sangeethamv <sangeethamv@gmail.com>
2015-09-14 11:19:23 +0530

    added new file to branch

---
+++ homeTitle.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+<title>SchoolGuide</title>
+	<?php
+	$profpic = "Images/gurukul.png";
+	?>
+
+<style type="text/css">
+body {
+	background-image: url('<?php echo $profpic;?>');
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+	width: 100%;
+	height: 100%;
+}
+</style>
+</head>
+
+<body>
+
+<h1><font color="white">School Guide!</font></h1>
+<p><font color="white">Schools of your neighbourhood.</font></p>
+
+</body>
+
+</html>
+
+
+
+
